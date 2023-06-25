7bae97cb592e043986aaebb3e1e4f3ee7e39fb65
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2023-06-25 15:10:48 +0000

    Update manual_sort.py

--- manual_sort.py
+++ manual_sort.py
@@ -9,10 +9,3 @@ def ask_user_cmp(item1, item2):
         if cmp == "2":
             return -1
         print("1 or 2, please!")
-
-ask_user_key = functools.cmp_to_key(ask_user_cmp)
-
-items_to_sort = ["pizza", "cheeseburger", "beef", "soup", "ice cream"]
-items_to_sort.sort(key=ask_user_key, reverse=True)
-print("Here's your list from highest to lowest:")
-print(items_to_sort)
